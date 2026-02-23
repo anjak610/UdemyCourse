7bcb1c9818e17981a3c78b6600932238d0928fe1
gladojo610 <gladojo610@gmail.com>
2026-02-23 13:25:16 +0700

    Making the code asynchronous

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786ef8c51f3c37bcee5f82a482ab050f18f3cd1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f3b1be3885bb5c92ef43ecb25ad5ec487ebe9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
