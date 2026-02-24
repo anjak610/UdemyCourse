a34b7af97bb2446333adafb8763822119f54eaec
gladojo610 <gladojo610@gmail.com>
2026-02-24 10:48:55 +0700

    Create the angular project

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bcb1c9818e17981a3c78b6600932238d0928fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad136984d058d22ce32e8b00deab1960148dd70")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
