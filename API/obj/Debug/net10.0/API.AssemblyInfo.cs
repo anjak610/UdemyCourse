ba6f5fe9f93c62fccc4fb025068b47e78f49b727
gladojo610 <gladojo610@gmail.com>
2026-02-24 12:48:50 +0700

    Running the Angular app over https

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef8b825c67efeb3f86b223b7fe08d495abf7c0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc9ad0c7afa50d23ecbe0c8c456ecc836a25dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
