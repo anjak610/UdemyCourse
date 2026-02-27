24e4417dc8cb591df133a0bdb299ba42c9f7b150
gladojo610 <gladojo610@gmail.com>
2026-02-27 12:18:28 +0700

    Creating a base API controller to avoid DRY

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6f5fe9f93c62fccc4fb025068b47e78f49b727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+badd90b0dc48acaaa734c7cb5837d0978eee5812")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
