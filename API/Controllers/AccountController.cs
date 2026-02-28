a5b18cdf7b69200e0f96a5eae2e9aea79408d0eb
gladojo610 <gladojo610@gmail.com>
2026-02-28 13:20:14 +0700

    Using extension methods

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,8 +3,8 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -30,13 +30,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-         return  new UserDto
-        {
-            Id = user.Id,
-            DisplayName = user.DisplayName,
-            Email = user.Email,
-            Token = tokenService.CreateToken(user)
-        }; 
+        return user.ToDto(tokenService);
     }
 
     [HttpPost("login")]
@@ -55,13 +49,8 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
             if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
-        return  new UserDto
-        {
-            Id = user.Id,
-            DisplayName = user.DisplayName,
-            Email = user.Email,
-            Token = tokenService.CreateToken(user)
-        }; 
+        return user.ToDto(tokenService);
+      
     }
 
     private async Task<bool> EmailExists(string email)
