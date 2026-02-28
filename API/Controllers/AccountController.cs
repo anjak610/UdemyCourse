f7f80368b8e07ea8b7f18045e1ad79a6e2a17627
gladojo610 <gladojo610@gmail.com>
2026-02-28 11:37:08 +0700

    Adding a Login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -14,8 +14,8 @@ public class AccountController(AppDbContext context): BaseApiController
     public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
         if(await EmailExists(registerDto.Email)) return BadRequest("Email already exists");
-        
-        using var hmac = new HMACSHA3_512();
+
+        using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
@@ -29,6 +29,25 @@ public class AccountController(AppDbContext context): BaseApiController
         await context.SaveChangesAsync();
 
         return user; 
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login (LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(user => user.Email == loginDto.Email);
+
+        if(user == null) return Unauthorized("Invalid Email address");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for(var i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return  user; 
     }
 
     private async Task<bool> EmailExists(string email)
