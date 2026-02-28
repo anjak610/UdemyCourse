8fe62d63d8690d8545d926acbceb8f8ab2284229
gladojo610 <gladojo610@gmail.com>
2026-02-28 12:21:03 +0700

    implementing the token logic

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f80368b8e07ea8b7f18045e1ad79a6e2a17627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c71072d27b3bffe7029833efda2024fa3115384")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
