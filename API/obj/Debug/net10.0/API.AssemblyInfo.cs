7ae093cb397f7fbaf8e1d17ac04c595ad043d411
gladojo610 <gladojo610@gmail.com>
2026-03-04 13:14:05 +0700

    Introduction to Angular service

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d21ea950018b11571ec3db9aa474cbdcff791d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021781391d4dda06a2ee066f4aad821e96a38470")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
