6b2c374150655519952609432dbdff62c08c2e16
gladojo610 <gladojo610@gmail.com>
2026-03-04 12:49:03 +0700

    Styling the nav bar

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b18cdf7b69200e0f96a5eae2e9aea79408d0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d21ea950018b11571ec3db9aa474cbdcff791d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
