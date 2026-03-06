21b275a200a0929b11b1986ada35d2e412d748f2
gladojo610 <gladojo610@gmail.com>
2026-03-06 10:10:28 +0700

    persisting the login with signals part 1

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae093cb397f7fbaf8e1d17ac04c595ad043d411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86573571cb91fbaa55b2d57ca7bf434fbeebfef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
