129ed975ddeb53f4fb497cd428a376b6387cd8b4
gladojo610 <gladojo610@gmail.com>
2026-03-06 11:19:08 +0700

    Adding a register form

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14505ddfe0af276b9eff9da91175e4e666c2f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23342291af6ff6c516972884aeb5995e20039682")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
