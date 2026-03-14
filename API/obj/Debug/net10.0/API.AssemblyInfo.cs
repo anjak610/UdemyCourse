57511be3f6ee898e53fb4ac38a0d73d5f8db4784
gladojo610 <gladojo610@gmail.com>
2026-03-14 14:57:39 +0700

    Adding more components for routing

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e976f27f91c6f8776ba0ebf51e166b4bff504ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91a4d7ba0250031493d696067457b43a77014ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
